b8bc405a8ae4dc9db08a85748186cb730a8b5d61
alex-hofsteede <alex@betterthanflex.com>
2011-05-27 00:09:48 -0700

    fixed CSS replacement for all types of CSS, set up links to <link>ed CSS and regex replace for inlines. Still need to handle @includes within CSS

--- swap/views.py
+++ swap/views.py
@@ -1,6 +1,6 @@
 from urlparse import urlparse, urljoin
-from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render_to_response, redirect, get_object_or_404
 from models import Page, CSSAsset
 import urllib2
 from django.template import RequestContext
@@ -25,7 +25,11 @@ def getpage(request):
     if request.method == 'POST':
         page_url = request.POST['url']
         try:
-            f =  urllib2.urlopen(page_url) 
+            #Check if urlparse can find a scheme for us, if not, we just put http:// in front
+            parsed = urlparse(page_url)
+            if parsed.scheme == '':
+                page_url = 'http://'+page_url
+            f =  urllib2.urlopen(page_url) #TODO rate limit this or find some way to stop ourselves from being used as a DOS tool
         except urllib2.HTTPError, error:
             return render_to_response('error.html',{'message':error.code}) 
         page_content = f.read()
@@ -44,7 +48,9 @@ def getpage(request):
             except urllib2.HTTPError, error:
                 continue
             css_content = f.read()
-            css_assets.append(createCSSAsset(css_content,page,css_url))
+            css_asset = createCSSAsset(css_content,page,css_url)
+            css_assets.append(css_asset)
+            css_link_tag['href'] = '/css/%s' % css_asset.uuid #No need to save a delimited value to regex out later. the link to /css/{uuid} will be constant
 
         css_style_tags = soup.findAll('style')
         for css_style_tag in css_style_tags:
@@ -56,14 +62,24 @@ def getpage(request):
         css_inline_style_tags = soup.findAll(style=True)
         for css_inline_style_tag in css_inline_style_tags:
             css_content = css_inline_style_tag['style']
-            css_assets.append(createCSSAsset(css_content,page))
+            css_asset = createCSSAsset(css_content,page)
+            css_assets.append(css_asset)#TODO css_assets is not necessary anymore but perhape we can save cycles by passing it diretly to editpage instead of having to get that all from the DB again
+            css_inline_style_tag['style'] =  delimiter + css_asset.uuid + delimiter 
         
         #save all the replacements to the page
         page.raw = unicode(soup)
         page.save()
+        return redirect("/editpage/%d" % page.id)
+    return HttpResponse('fail')#TODO return something good 
 
-        return render_to_response('edit.html', {'page':page,'stylesheets':css_assets}, context_instance=RequestContext(request))
-    return '' 
+def getCSS(request,uuid):
+    css = get_object_or_404(CSSAsset,uuid=uuid)
+    return HttpResponse(css.raw)
+
+def editpage(request,page_id):
+    page = Page.objects.get(pk=page_id) #TODO use uuids
+    stylesheets = CSSAsset.objects.filter(page=page.id)
+    return render_to_response('edit.html', {'page':page,'stylesheets':stylesheets}, context_instance=RequestContext(request))
 
 #TODO move util functions like this out of views. 
 def getCSSByUUID(uuid):
@@ -73,17 +89,17 @@ def savepage(request,page_id):
 #TODO all kinds of checks / tests needed here so we don't get pwned
     if request.method == 'POST':
         for key,value in request.POST.items():
-            match = re.search(r'cssasset_([a-fA-F0-9]+)',key)
+            match = re.search(r'cssasset_([0-f]+)',key)
             if match:
                 css_asset = getCSSByUUID(match.group(1))
                 if css_asset:
                     css_asset.raw = value
                     css_asset.save()
-    return showpage(request,page_id)#TODO redirect to named view here
+    return redirect("/showpage/%s" % page_id)
 
 def showpage(request,page_id):
     page = Page.objects.get(pk=page_id) #TODO check for error 
     def replacementText(match):
         return getCSSByUUID(match.group(1)).raw
-    html = re.sub(delimiter+r'([a-fA-F0-9]+)'+delimiter,replacementText,page.raw)
+    html = re.sub(delimiter+r'([0-f]+)'+delimiter,replacementText,page.raw)
     return HttpResponse(html)
--- urls.py
+++ urls.py
@@ -16,8 +16,12 @@ urlpatterns = patterns('',
 
     url(r'^/?$', swap.views.index),
     url(r'^getpage/?$', swap.views.getpage),
+    url(r'^editpage/(?P<page_id>\d+)/?', swap.views.editpage),
     url(r'^savepage/(?P<page_id>\d+)/?', swap.views.savepage),
     url(r'^showpage/(?P<page_id>\d+)/?', swap.views.showpage),
+
+    #get CSS by UUID
+    url(r'^css/(?P<uuid>[0-f]+)/?', swap.views.getCSS),
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
