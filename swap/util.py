28c16ceff3d1b0f9b20aee48dc1eee0cd123bc11
alex-hofsteede <alex@betterthanflex.com>
2011-07-10 00:20:36 -0700

    added comments and cleanup

--- swap/util.py
+++ swap/util.py
@@ -11,38 +11,11 @@ from pygments.token import Token, Text, Comment, Operator, Keyword, Name, String
 
 delimiter = '--REPLACE--'
 
-def createCSSAsset(content,page,url='',name=''):
-    css_asset = CSSAsset.create()
-    css_asset.url = url
-    css_asset.original = content
-    css_asset.raw = content
-    css_asset.page = page
-    css_asset.name = name
-    css_asset.save()
-    return css_asset 
-
-def makeLinksAbsolute(document, attrs, root_url):
-    if type(attrs) is str:
-        attrs = [attrs]
-    attr_regex = re.compile('|'.join(attrs)+r'\s*=',re.I)
-    output_tokens = []
-    tokens = HtmlLexer().get_tokens_unprocessed(document)
-    for index,token,value in tokens:
-        output_tokens.append(value)
-        if token == Token.Name.Attribute and attr_regex.match(value):
-            index,token,value = tokens.next() # get the attribute value
-            output_tokens.append('"'+urljoin(root_url,value.strip("\"' "))+'"')
-    return u''.join(output_tokens)
-
-def makeCSSURLsAbsolute(css_content,root_url):
-    regex = re.compile(r'''((?:@import\s+(?:url\(?)?|\burl\()\s*['"]?)((?:[^'"()\\]|\\.)*)(['"]?\s*\)?)''',re.I)
-    #regex = re.compile(r'''\burl\(\s*['"]?((?:[^'"()\\]|\\.)*)['"]?\s*\)''',re.I)
-    def replace(match):
-        #print "matched %s : making absolute" % match.group(0)
-        return match.group(1) + unicode(urljoin(root_url,match.group(2))) + match.group(3)
-    return regex.sub(replace,css_content)
-
 def processPage(page_url):
+    """
+    Main entry point in this class, Grabs a web page given a URL and parses out all the styles, 
+    creating classes for each one
+    """
     try:
         #Check if urlparse can find a scheme for us, if not, we just put http:// in front
         parsed = urlparse(page_url)
@@ -75,42 +48,21 @@ def processPage(page_url):
     page.save()
     css_assets = []
 
-    #print page_content
     page_content = makeLinksAbsolute(page_content,[u'href',u'src'], page_url)
     page_content = parseStyleAttributes(page_content, css_assets, page)
     page_content = parseStyleTags(page_content, css_assets, page)
     page_content = parseLinkedStylesheets(page_content, css_assets, page)
-    #print page_content
 
     #save all the replacements to the page
     page.raw = page_content
     page.save()
     return page
 
-def parseTagAttributes(tokens):
-    attr_dict = {}
-    attr_regex = re.compile(r'([a-zA-Z0-9_:-]+)(\s*=)?')
-    end_regex = re.compile(r'(/?\s*>)')
-    for index,token,value in tokens:
-        if token == Token.Name.Attribute :
-            attr_match = attr_regex.match(value)
-            if attr_match.group(2):
-                index,token,value = tokens.next() # get the attribute value
-                attr_dict[attr_match.group(1).lower()] = value.strip("\"' ")
-            else:
-                attr_dict[attr_match.group(1).lower()] = None
-        elif token != Token.Name.Attribute:
-            close_match = end_regex.match(value)
-            if close_match:
-                return (attr_dict,close_match.group(1))
-
-def serializeTagAttributes(attr_dict):
-    #TODO is there a way to join with a separator on the beginning and end?
-    return " " + " ".join(k + (('="' + v + '"') if v != None else '') for k,v in attr_dict.iteritems()) + " " 
-
 def parseStyleAttributes(document, css_assets, page):
-    #Grabs any style="" attributes on normal html tags and saves the CSS therein.
-    #replace with uuid, same as above
+    """
+    Grabs any style="" attributes on normal html tags and saves the CSS therein.
+    Replaces the style with a delimited UUID tag that we can use later to re-insert the style
+    """
     attr_regex = re.compile(r'style\s*=',re.I)
     output_tokens = []
     tokens = HtmlLexer().get_tokens_unprocessed(document)
@@ -126,8 +78,10 @@ def parseStyleAttributes(document, css_assets, page):
     return "".join(output_tokens)
 
 def parseStyleTags(document, css_assets, page):
-    #Grabs any <style> tags and saves the CSS therein. replaces with a
-    #uuid that we can then regex out later and replace with modified css
+    """
+    Grabs any <style> tags and saves the CSS therein. replaces with a
+    uuid that we can use later to re-insert the style. 
+    """
     output_tokens = []
     tokens = HtmlLexer().get_tokens_unprocessed(document)
     intag = False
@@ -155,10 +109,11 @@ def parseStyleTags(document, css_assets, page):
             stylesheet_tokens.append(value)
     return "".join(output_tokens)
 
-
 def parseLinkedStylesheets(document, css_assets, page):
-    #Grabs any <link> tags that point to stylesheets, downloads and saves the 
-    #linked stylesheet, and replaces the link with our own custom link
+    """
+    Grabs any <link> tags that point to stylesheets, downloads and saves the 
+    linked stylesheet, and replaces the link to our own saved version
+    """
     output_tokens = []
     tokens = HtmlLexer().get_tokens_unprocessed(document)
     tag_regex = re.compile(r'<\s*link',re.I)
@@ -180,20 +135,19 @@ def parseLinkedStylesheets(document, css_assets, page):
                 css_assets.append(css_asset)
                 attr_dict['href'] = u'/css/%s' % css_asset.uuid #No need to save a delimited value to regex out later. the link to /css/{uuid} will be constant
                 parseNestedStylesheets(css_asset, css_assets, page)
-            output_tokens.append(serializeTagAttributes(attr_dict))
+            output_tokens.append(" " + serializeTagAttributes(attr_dict) + " ")
             output_tokens.append(close_tag)
     return "".join(output_tokens)
 
-
-
 def parseNestedStylesheets(css_asset, css_assets, page):
-    #Ideally we would have had just 1 group matching the URL, with everything else in lookaheads
-    # and lookbehinds, unfortunately the lookbehind has to be fixed width and we are matching a
-    #variable amount of whitespace. So group(1) is everything between the @import and the actual URL
-    #and group(2) is the URL, and group(3) is any trailing characers
+    """
+    Looks through a CSS stylesheet for any @import tags and downloads the imported stylesheets, 
+    replacing their reference in the parent stylesheet with the link to our own saves version
+    """
+    #Group(1) is everything between the @import and the actual URL,
+    #group(2) is the URL, and group(3) is any trailing characers
     regex = re.compile(r'''(?<=@import)(\s+(?:url)?\(?\s*['"]?)((?:[^'"()\\]|\\.)*)(['"]?\s*\)?)''',re.I)
-    #This replacement function gets called on every match and downloads/parses the stylesheet 
-    #at that location.
+    #This replacement function gets called on every match and downloads/parses the stylesheet at that location.
     #TODO we might want to do this asynchronously
     def replace(match):
         css_url = match.group(2)
@@ -210,4 +164,76 @@ def parseNestedStylesheets(css_asset, css_assets, page):
     css_asset.raw = regex.sub(replace,css_asset.raw)
     css_asset.save()
 
+
+def createCSSAsset(content,page,url='',name=''):
+    """
+    Creates a CSSAsset class from the given values
+    """
+    css_asset = CSSAsset.create()
+    css_asset.url = url
+    css_asset.original = content
+    css_asset.raw = content
+    css_asset.page = page
+    css_asset.name = name
+    css_asset.save()
+    return css_asset 
+
+def makeLinksAbsolute(document, attrs, root_url):
+    """
+    Looks through document for tags with attributes in attrs, and uses urljoin to make those
+    attribute values into absolute URLs.
+    """
+    if type(attrs) is str:
+        attrs = [attrs]
+    attr_regex = re.compile('|'.join(attrs)+r'\s*=',re.I)
+    output_tokens = []
+    tokens = HtmlLexer().get_tokens_unprocessed(document)
+    for index,token,value in tokens:
+        output_tokens.append(value)
+        if token == Token.Name.Attribute and attr_regex.match(value):
+            index,token,value = tokens.next() # get the attribute value
+            output_tokens.append('"'+urljoin(root_url,value.strip("\"' "))+'"')
+    return u''.join(output_tokens)
+
+def makeCSSURLsAbsolute(css_content,root_url):
+    """
+    Looks through a CSS document for any @import or url() rules, and uses urljoin to change the URL 
+    into an absolute one
+    """
+    regex = re.compile(r'''((?:@import\s+(?:url\(?)?|\burl\()\s*['"]?)((?:[^'"()\\]|\\.)*)(['"]?\s*\)?)''',re.I)
+    #regex = re.compile(r'''\burl\(\s*['"]?((?:[^'"()\\]|\\.)*)['"]?\s*\)''',re.I)
+    def replace(match):
+        #print "matched %s : making absolute" % match.group(0)
+        return match.group(1) + unicode(urljoin(root_url,match.group(2))) + match.group(3)
+    return regex.sub(replace,css_content)
+
+def parseTagAttributes(tokens):
+    """
+    This is the first step to building a full parser from our lexer, while in an HTML tag, this function saves
+    all attributes and their values into a dictionary, until we reach the end of the tag. It returns the attribute
+    dictionary and the closing tag. Tokens is an iterator produced by the lexer whose current position should be at 
+    the beginning of a tag, having just consumed the tag name. 
+    """
+    attr_dict = {}
+    attr_regex = re.compile(r'([a-zA-Z0-9_:-]+)(\s*=)?')
+    end_regex = re.compile(r'(/?\s*>)')
+    for index,token,value in tokens:
+        if token == Token.Name.Attribute :
+            attr_match = attr_regex.match(value)
+            if attr_match.group(2):
+                index,token,value = tokens.next() # get the attribute value
+                attr_dict[attr_match.group(1).lower()] = value.strip("\"' ")
+            else:
+                attr_dict[attr_match.group(1).lower()] = None
+        elif token != Token.Name.Attribute:
+            close_match = end_regex.match(value)
+            if close_match:
+                return (attr_dict,close_match.group(1))
+
+def serializeTagAttributes(attr_dict):
+    """
+    Takes an attribute dictionary produced by parseTagAttributes, and returns the HTML string representation
+    of those attributes
+    """
+    return " ".join(k + (('="' + v + '"') if v != None else '') for k,v in attr_dict.iteritems())
 
